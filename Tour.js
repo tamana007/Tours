315d222c5ffd04dfd6dbc04a57a3c8dead3ebbf6
tamana007 <tamana.efatwira1@gmail.com>
2023-10-12 14:31:19 -0700

    new commit

--- Tour.js
+++ Tour.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 
 const Tour = ({id,image,info,price,name}) => {
   return <article className='single-tour'>
-    {/* <img src={image} alt='name'> */}
+    {/* <img src='http://www.visitsoutheastengland.com/imageresizer/?image=%2fdbimgs%2fgardens.jpg&action=MediaGallery'/> */}
+    <img src={image}></img>
       <footer>
         <div className='tour-info'>
           <h4>{name}</h4>
           <h4 className='tour-price'>{price}</h4>
+          {/* <h4 className='tour-price'>{price}</h4> */}
         </div>
+        <p>{info}</p>
+        <button className='delete-btn'>Not interested</button>
       </footer>
+     
 
   </article>
 };
